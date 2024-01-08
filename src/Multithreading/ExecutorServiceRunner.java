4d50e8bfa1529924eb273d6d952d0a7bcdf25ffc
Rohith Goli <rohithgoli396@gmail.com>
2024-01-08 10:44:53 +0530

    feat: added FixedThreadPool of ExecutorService

--- src/Multithreading/ExecutorServiceRunner.java
+++ src/Multithreading/ExecutorServiceRunner.java
@@ -3,22 +3,43 @@ package Multithreading;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
+class Task extends Thread {
+	
+	private int index;
+	
+	public Task(int index) {
+		this.index = index;
+	}
+	
+	public void run() {
+		System.out.println("\n Task "+ index + " started");
+		
+		for(int count = 100*index; count <= 100*index + 99; count++) {
+			System.out.print(count + " ");
+		}
+		System.out.println("\n Task "+ index + " ended");
+	}
+}
+
+
 public class ExecutorServiceRunner {
 	
 	public static void main(String[] args) {
 		
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		executorService.execute(new Task1());
-		executorService.execute(new Thread(new Task2()));
+//		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		
-		System.out.println("\n Task3 kicked off");
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
 		
-		// Task3
-		for(int i=301; i<=399; i++) {
-			System.out.print(i + " ");
-		}
-		System.out.println("\n Task3 Done");
-		
+		executorService.execute(new Task(1));
+		executorService.execute(new Task(2));
+		executorService.execute(new Task(3));
+		executorService.execute(new Task(4));
+		executorService.execute(new Task(5));
+		executorService.execute(new Task(6));
+		executorService.execute(new Task(7));
+		executorService.execute(new Task(8));
+
 		executorService.shutdown();
 	}
 }
